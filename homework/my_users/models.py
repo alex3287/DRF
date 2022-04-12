83052b251ab67a516914ff4e08fe52d04cd2e50b
Alexander Mangazeev <alex3287@bk.ru>
2022-04-12 21:12:53 +0300

    create app my_user

---
+++ homework/my_users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class My_User(AbstractUser):
+    username = models.CharField(verbose_name='username', max_length=64)
+    first_name = models.CharField(verbose_name='first name', max_length=64)
+    last_name = models.CharField(verbose_name='last name', max_length=64)
+    email = models.EmailField(verbose_name='email', unique=True)
+
+    USERNAME_FIELD = 'email'
