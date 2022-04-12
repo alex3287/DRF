6340aabb73d2aefb6dc19a1a19e1f957e18c523e
Alexander Mangazeev <alex3287@bk.ru>
2022-04-12 21:41:51 +0300

    problem

--- homework/my_users/models.py
+++ homework/my_users/models.py
@@ -6,7 +6,7 @@ class MyUser(AbstractUser):
     username = models.CharField(verbose_name='username', max_length=64)
     first_name = models.CharField(verbose_name='first name', max_length=64)
     last_name = models.CharField(verbose_name='last name', max_length=64)
-    # email = models.EmailField(verbose_name='email', unique=True)
+    email = models.EmailField(verbose_name='email', unique=True)
 
-    # USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
