0a53d9c12caca610ea765d299fe5e9db1f1e5cf5
administrator <administrator@gym11.local>
2022-04-13 10:37:55 +0300

    change serializers

--- homework/my_users/serializers.py
+++ homework/my_users/serializers.py
@@ -5,4 +5,6 @@ from .models import MyUser
 class MyUserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = MyUser
-        fields = '__all__'
+        # fields = '__all__'
+        # fields = ['url', 'username', 'email', 'groups']
+        fields = ['url', 'username', 'first_name', 'last_name', 'email']
