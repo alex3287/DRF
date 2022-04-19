bc309f52c44742a4184d1df5263d030c43416377
Alexander Mangazeev <alex3287@bk.ru>
2022-04-19 20:36:37 +0300

    lesson 4

--- homework/my_projects/views.py
+++ homework/my_projects/views.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Project, ToDo
 from .serializers import ProjectSerializer, ToDoSerializer
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+from .filters import ProjectFilter, ToDoFilter
 
 
 class ToDoLimitPagination(LimitOffsetPagination):
@@ -20,9 +22,18 @@ class ProjectViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
     pagination_class = ProjectLimitPagination
+    filter_class = ProjectFilter
 
 
 class ToDoViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
     pagination_class = ToDoLimitPagination
+    filter_class = ToDoFilter
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.status = 'закрыто'
+        self.perform_create(instance)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
